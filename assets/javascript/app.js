f77eb7111c36d222c17ee085136fb0ed64e2f622
nmiesmer <nic.miesmer@segmint.com>
2018-09-03 19:27:19 -0400

    some basic functions for play

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,25 @@
-// 1. Set Up Timer
 
-// 2. set up one html form?? 
+var playerAnswers = [""];
+var correctAnswers = [true];
+
+
+
+//call from click event
+var answerQuestion = function() {
+
+    //click button add add it's value to the playerAnswer array
+    //some how record button's question number, to use as index.
+}
+
+
+//call when finish button is clicked
+//or time runs out
+var finish = function() {
+     
+}
+
+//
+var checkAnswers = function(){
+    //compare correctAnswers array vs playerAnswers array
+
+}
