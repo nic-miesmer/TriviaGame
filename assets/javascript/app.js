40213ea36173ea0ae985599d0fc3ba64fef5ee7f
nmiesmer <nic.miesmer@segmint.com>
2018-09-04 21:16:02 -0400

    Finish function displays how many correct

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,7 @@ var correctCount = 0;
 var answerQuestion = function(event) {
 
     playerAnswers[event.currentTarget.id] = event.currentTarget.value;
-
+    console.log
     //click button add add it's value to the playerAnswer array
     //some how record button's question number, to use as index.
 }
@@ -24,10 +24,18 @@ var finish = function() {
             correctCount++;
         }
     }
+
+
+    var p = $('<p>');
+    p.text("You got " + correctCount + "/" + correctAnswers.length + " correct")
     console.log(correctCount);
+    $("#correctAnswers").append(p);
+
 
      
 }
+
+
 
 
 $('#buttons1').on ('click','button', function(event){
@@ -36,6 +44,11 @@ $('#buttons1').on ('click','button', function(event){
   });  
 
   $('#buttons2').on ('click','button', function(event){
+    answerQuestion(event);
+   console.log(this.value)
+  });  
+
+  $('#buttons3').on ('click','button', function(event){
     answerQuestion(event);
    console.log(this.value)
   });  
