6a70ce5e41a8be0fd3117d23e90bd5e8bde26f11
nmiesmer <nic.miesmer@segmint.com>
2018-09-04 21:09:36 -0400

    added questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,15 @@
 
-var playerAnswers = [""];
-var correctAnswers = ["a"];
+var playerAnswers = ["", "", ""];
+var correctAnswers = ["a", "b", "c"];
+var correctCount = 0;
+
 
 
 
 //call from click event
 var answerQuestion = function(event) {
 
-
-    console.log(event.currentTarget.value);
-
     playerAnswers[event.currentTarget.id] = event.currentTarget.value;
-
-    console.log(playerAnswers)
 
     //click button add add it's value to the playerAnswer array
     //some how record button's question number, to use as index.
@@ -22,17 +19,28 @@ var answerQuestion = function(event) {
 //call when finish button is clicked
 //or time runs out
 var finish = function() {
+    for(var i = 0; i<correctAnswers.length; i++){
+        if(correctAnswers[i] === playerAnswers[i]){
+            correctCount++;
+        }
+    }
+    console.log(correctCount);
+
      
-}
-
-//
-var checkAnswers = function(){
-    //compare correctAnswers array vs playerAnswers array
-
 }
 
 
 $('#buttons1').on ('click','button', function(event){
     answerQuestion(event);
    console.log(this.value)
+  });  
+
+  $('#buttons2').on ('click','button', function(event){
+    answerQuestion(event);
+   console.log(this.value)
+  });  
+
+  $('#finishButton').on ('click','button', function(event){
+    finish();
+//    console.log(this.value)
   });  
